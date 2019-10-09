3086d66e6d3be50cc7dc2d9e1e2f9ec202977918
Lendy Mulot <56113041+ZeGmX@users.noreply.github.com>
2019-10-09 10:02:26 +0200

    bonus + fin de la partie obligatoire

--- 7colors.c
+++ 7colors.c
@@ -1,6 +1,6 @@
 /* Template of the 7 wonders of the world of the 7 colors assigment. */
 
-#include <stdio.h>     /* printf */
+#include <stdio.h>     // printf //
 #include <stdlib.h>     //random
 #include <time.h>     //to initialize random
 
@@ -21,7 +21,7 @@ enum colors {
 typedef struct board_game board_game;
 
 struct board_game {
-  char* game;       //the board game
+  enum colors *game; //the board game
   enum colors turn; //the player who has to play next
   int unused;       //the number of positions which doesn't belong to any player
   int nb_player1;   //number of positions that player 1 has conquered
@@ -39,22 +39,20 @@ struct board_game {
  *     For this first assignment, no dinosaure will get you if you do that.
  */
 
-char tab[BOARD_SIZE * BOARD_SIZE] = { 0 }; // Filled with zeros
-board_game board = {tab, PLAYER1, BOARD_SIZE * BOARD_SIZE - 2, 1, 1};
 
 
 
 
 /** Retrieves the color of a given board cell */
-enum colors get_cell(int x, int y)
+enum colors get_cell(int x, int y, board_game *board)
 {
-    return tab[y * BOARD_SIZE + x];
+    return board->game[y * BOARD_SIZE + x];
 }
 
 /** Changes the color of a given board cell */
-void set_cell(int x, int y, enum colors color)
+void set_cell(int x, int y, enum colors color, board_game *board)
 {
-    tab[y * BOARD_SIZE + x] = color;
+    board->game[y * BOARD_SIZE + x] = color;
 }
 
 //Allows us to transform a color into its char representation
@@ -103,6 +101,8 @@ int colors_to_int(enum colors color) {
       return 7;
     case PLAYER2:
       return 8;
+    default:
+    return -1;
   }
 }
 
@@ -111,102 +111,107 @@ int colors_to_int(enum colors color) {
  * Implementation note: It would be nicer to do this with ncurse or even
  * SDL/allegro, but this is not really the purpose of this assignment.
  */
-void print_board(void)
+void print_board(board_game *board)
 {
     int i, j;
     for (i = 0; i < BOARD_SIZE; i++) {
         for (j = 0; j < BOARD_SIZE; j++) {
-          enum colors color =  get_cell(i, j);
+          enum colors color =  get_cell(j, i, board);
           printf("%c", colors_to_char(color));
         }
         printf("\n");
     }
 }
 
-void init_board() { //initialize the board by picking a random color for each position
-  for (int x = 0 ; x < BOARD_SIZE ; x++) {
-    for (int y = 0 ; y < BOARD_SIZE ; y++) {
+board_game init_board() { //initialize the board by picking a random color for each position
+  static enum colors tab[BOARD_SIZE * BOARD_SIZE] = {(enum colors) A}; //DO NOT REMOVE THE STATIC. OTHERWHISE YOU COULD EXPECT HOURS OF HEAD SCRATCHING
+  for (int y = 0 ; y < BOARD_SIZE ; y++) {
+    for (int x = 0 ; x < BOARD_SIZE ; x++) {
       int color_index = rand() % NB_COLORS;
       enum colors color = A;
       color += color_index;
-      set_cell(x, y, color);
+      tab[y * BOARD_SIZE + x] = color;
     }
   }
-  set_cell(0, BOARD_SIZE - 1, PLAYER1); //first player
-  set_cell(BOARD_SIZE - 1, 0, PLAYER2); //second player
+  enum colors p1 = PLAYER1;
+  enum colors p2 = PLAYER2;
+  tab[BOARD_SIZE - 1] = p1;//first player
+  tab[(BOARD_SIZE - 1) * BOARD_SIZE] = p2;//second player
+  board_game board = {tab, p1, BOARD_SIZE * BOARD_SIZE - 2, 1, 1};
+  return board;
 }
 
 //update one position next to (x, y) in the direction (dx, dy) where dx + dy = 1, dxdy = 0
-void update_one_dir(int x, int y, int dx, int dy, enum colors color_played, int* has_done_something) {
-  if (x + dx < BOARD_SIZE && x + dx >= 0 && y + dy < BOARD_SIZE && y + dy >= 0 && get_cell(x + dx, y + dy) == color_played) { //checking if the position isn't out of the board
-    set_cell(x + dx, y + dy, board.turn);
+void update_one_dir(int x, int y, int dx, int dy, enum colors color_played, int* has_done_something, board_game *board) {
+  if (x + dx < BOARD_SIZE && x + dx >= 0 && y + dy < BOARD_SIZE && y + dy >= 0 && get_cell(x + dx, y + dy, board) == color_played) { //checking if the position isn't out of the board
+    set_cell(x + dx, y + dy, board->turn, board);
     *has_done_something = 1;
-    board.unused--;
-    if (board.turn == PLAYER1) {
-      board.nb_player1++;
+    board->unused--;
+    if (board->turn == PLAYER1) {
+      board->nb_player1++;
     }
     else {
-      board.nb_player2++;
+      board->nb_player2++;
     }
   }
 }
 
-int update_around_cell (int x, int y, enum colors color_played) { //returns 1 if something has changed, 0 otherwhise
+int update_around_cell (int x, int y, enum colors color_played, board_game *board) { //returns 1 if something has changed, 0 otherwhise
   int has_done_something = 0;
-  if (get_cell(x, y) == board.turn) {
-    update_one_dir(x, y, -1, 0, color_played, &has_done_something);
-    update_one_dir(x, y, 1, 0, color_played, &has_done_something);
-    update_one_dir(x, y, 0, -1, color_played, &has_done_something);
-    update_one_dir(x, y, 0, 1, color_played, &has_done_something);
+  if (get_cell(x, y, board) == board->turn) {
+    update_one_dir(x, y, -1, 0, color_played, &has_done_something, board);
+    update_one_dir(x, y, 1, 0, color_played, &has_done_something, board);
+    update_one_dir(x, y, 0, -1, color_played, &has_done_something, board);
+    update_one_dir(x, y, 0, 1, color_played, &has_done_something, board);
   }
   return has_done_something;
 }
 
-void update_board(enum colors color_played) { //player = "^" ou "v" et color_played = A...G
+void update_board(enum colors color_played, board_game *board) { //player = "^" ou "v" et color_played = A...G
   int has_done_something = 1;
   while (has_done_something) {
     has_done_something = 0;
     for (int x = 0 ; x < BOARD_SIZE ; x++) {
       for (int y = 0 ; y < BOARD_SIZE ; y++) {
-        has_done_something = has_done_something || update_around_cell(x, y, color_played);
+        has_done_something = has_done_something || update_around_cell(x, y, color_played, board);
       }
     }
   }
-  if (board.turn == PLAYER1) {
-    board.turn = PLAYER2;
+  if (board->turn == PLAYER1) {
+    board->turn = PLAYER2;
   }
   else {
-    board.turn = PLAYER1;
+    board->turn = PLAYER1;
   }
 }
 
-void full_game(enum colors (*p1)(), enum colors (*p2)()) { //game between two payers p1 and p2
+void full_game(enum colors (*p1)(), enum colors (*p2)(), board_game *board) { //game between two payers p1 and p2 with the game board
   int k = 0;
   enum colors color;
-  while(board.nb_player1 <= BOARD_SIZE * BOARD_SIZE / 2 && board.nb_player2 <= BOARD_SIZE * BOARD_SIZE / 2) {
+  while(board->nb_player1 <= BOARD_SIZE * BOARD_SIZE / 2 && board->nb_player2 <= BOARD_SIZE * BOARD_SIZE / 2) {
     printf("\n\n");
     if (k == 0) {
-      color = p1();
+      color = p1(board);
     }
     else {
-      color = p2();
+      color = p2(board);
     }
     printf("player %d played color %c\n", k + 1, colors_to_char(color));
-    update_board(color);
-    print_board();
-    printf("Player 1 has conquered %d percent(s)\n", board.nb_player1 * 100 / (BOARD_SIZE * BOARD_SIZE));
-    printf("Player 2 has conquered %d percent(s)\n", board.nb_player2 * 100 / (BOARD_SIZE * BOARD_SIZE));
+    update_board(color, board);
+    print_board(board);
+    printf("Player 1 has conquered %d percent(s)\n", board->nb_player1 * 100 / (BOARD_SIZE * BOARD_SIZE));
+    printf("Player 2 has conquered %d percent(s)\n", board->nb_player2 * 100 / (BOARD_SIZE * BOARD_SIZE));
     k = (k + 1) % 2;
   }
-  if (board.nb_player1 > board.nb_player2) {
-    printf("Player 1 won the game! with %d percents of the board\n", 100 * board.nb_player1 / (BOARD_SIZE * BOARD_SIZE));
+  if (board->nb_player1 > board->nb_player2) {
+    printf("Player 1 won the game! with %d percents of the board\n", 100 * board->nb_player1 / (BOARD_SIZE * BOARD_SIZE));
   }
   else {
-    printf("Player 2 won the game! with %d percents of the board\n", 100 * board.nb_player2 / (BOARD_SIZE * BOARD_SIZE));
+    printf("Player 2 won the game! with %d percents of the board\n", 100 * board->nb_player2 / (BOARD_SIZE * BOARD_SIZE));
   }
 }
 
-enum colors player_random() {  //player who randomly chooses a color each turn
+enum colors player_random(board_game board) {  //player who randomly chooses a color each turn
   int color_index = rand() % NB_COLORS;
   enum colors color = A;
   color += color_index;
@@ -214,7 +219,7 @@ enum colors player_random() {  //player who randomly chooses a color each turn
 
 }
 
-enum colors player_human() { //pretty self explanatory
+enum colors player_human(board_game board) { //pretty self explanatory
   int color_index = 0;
   do {
     printf("Enter a number between 1 and 7 (for A to G) : ");
@@ -224,24 +229,24 @@ enum colors player_human() { //pretty self explanatory
   return color + (color_index - 1);
 }
 
-void add_adjacent_colors(int x, int y, int dx, int dy, int *colors_available) {
+void add_adjacent_colors(int x, int y, int dx, int dy, int *colors_available, board_game *board) {
   if (x + dx < BOARD_SIZE && x + dx >= 0 && y + dy < BOARD_SIZE && y + dy >= 0) { //checking if the position isn't out of the board
-    int i = colors_to_int(get_cell(x + dx, y + dy));
+    int i = colors_to_int(get_cell(x + dx, y + dy, board));
     colors_available[i] = 1;
   }
 }
 
 
-enum colors player_smart_random() { //player who randomly chooses a color among the ones that he can reach
+enum colors player_smart_random(board_game *board) { //player who randomly chooses a color among the ones that he can reach
   int colors_available[9] = {0};
   int nb_colors = 0;
   for (int x = 0 ; x < BOARD_SIZE ; x++) {    //we keep track of which colors whe can access and which we can't
     for (int y = 0 ; y < BOARD_SIZE ; y++) {
-      if (get_cell(x, y) == board.turn) {
-        add_adjacent_colors(x, y, -1, 0, colors_available);
-        add_adjacent_colors(x, y, 1, 0, colors_available);
-        add_adjacent_colors(x, y, 0, -1, colors_available);
-        add_adjacent_colors(x, y, 0, 1, colors_available);
+      if (get_cell(x, y, board) == board->turn) {
+        add_adjacent_colors(x, y, -1, 0, colors_available, board);
+        add_adjacent_colors(x, y, 1, 0, colors_available, board);
+        add_adjacent_colors(x, y, 0, -1, colors_available, board);
+        add_adjacent_colors(x, y, 0, 1, colors_available, board);
       }
     }
   }
@@ -264,7 +269,8 @@ enum colors player_smart_random() { //player who randomly chooses a color among 
     return color + (index - 1);
   }
   else {  //we can't acess any color (the other player is blocking the way), so any color will have no effect
-    return A;
+    enum colors res = A;
+    return res;
   }
 }
 
@@ -276,8 +282,8 @@ int main(void)
 	   "Current board state:\n");
 
     srand(time(NULL)); //initialization of the random sequence
-    init_board();
-    print_board();
-    full_game(player_random, player_smart_random);
+    board_game board = init_board();
+    print_board(&board);
+    full_game(player_human, player_smart_random, &board);
     return 0; // Everything went well
 }
